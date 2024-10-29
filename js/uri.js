3eb090fd50c931dd0e42665e02dfdb337f010191
TheRev <cesarhumbertogarciafelix@gmail.com>
2024-10-29 13:57:22 -0700

    Examen Api

---
+++ js/uri.js
@@ -0,0 +1,13 @@
+﻿const perroActualElement = document.getElementById("perroActual");
+const API = "https://dog.ceo/api/breeds/image/random";
+
+document.getElementById("siguiente").addEventListener("click", nuevoPerro);
+async function nuevoPerro() {
+    perroActualElement.classList.toggle("escondido", true);
+    const res = await fetch(API);
+    const resJson = await res.json();
+    perroActualElement.src = resJson.message;
+    perroActualElement.classList.toggle("escondido", false);
+}
+//Ejecutado
+nuevoPerro();
